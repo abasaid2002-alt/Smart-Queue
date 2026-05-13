3baede009f2e0a35b76bc78f6490d43b5286aed8
Abanob <aba.said2002@gmail.com>
2026-05-13 12:38:36 +0200

    Initial backend project setup

---
+++ src/test/java/abanobsaid/Smart/Queue/SmartQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package abanobsaid.Smart.Queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
